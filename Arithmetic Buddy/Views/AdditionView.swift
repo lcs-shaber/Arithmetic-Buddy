41a8cefdc6a6a78e1fd74708790826cfc43be819
Sophie Haber <SHaber@lcs.on.ca>
2024-02-02 09:21:22 -0500

    Created the project and finished initial project organization.

---
+++ Arithmetic Buddy/Views/AdditionView.swift
@@ -0,0 +1,18 @@
+//
+//  AdditionView.swift
+//  Arithmetic Buddy
+//
+//  Created by Sophie Haber on 02.02.24.
+//
+
+import SwiftUI
+
+struct AdditionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AdditionView()
+}
