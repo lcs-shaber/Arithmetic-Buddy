a4d74d295b9a3264edd41d6fa4b352436145b5cd
Sophie Haber <SHaber@lcs.on.ca>
2024-02-02 09:18:32 -0500

    Initial Commit

---
+++ Arithmetic Buddy/Arithmetic_BuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Arithmetic_BuddyApp.swift
+//  Arithmetic Buddy
+//
+//  Created by Sophie Haber on 02.02.24.
+//
+
+import SwiftUI
+
+@main
+struct Arithmetic_BuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
